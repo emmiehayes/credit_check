09489d8470a4e813df9eb560cf2ddff4145bff96
Emmie Hayes <emmiehayes2@gmail.com>
2018-05-06 10:13:24 -0600

    Validate one card number

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class CreditCheck
 
   attr_reader :card_number
@@ -9,5 +11,30 @@ class CreditCheck
   def card_number_to_reversed_array
     @card_number.digits
   end
+
+  def double_every_other_number
+    card_number_to_reversed_array.map.with_index do |num, index|
+      index.odd? ? num * 2 : num
+    end
+  end
+
+  def sum_numbers_over_9
+    double_every_other_number.map do |num|
+      num > 9 ? num - 9 : num
+    end
+  end
+
+  def sum_final_array
+    sum_numbers_over_9.sum
+  end
+
+  def validate
+    if sum_final_array % 10 == 0
+      "Card is valid."
+    else
+      "Card is invalid."
+    end
+  end
+
 
 end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -20,6 +20,29 @@ class CreditCheckTest < Minitest::Test
     assert_equal expected, cc.card_number_to_reversed_array
   end
 
+  def test_it_can_double_every_other_number
+    cc = CreditCheck.new("4929735477250543")
+    expected = [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8]
+    assert_equal expected, cc.double_every_other_number
+  end
+
+  def test_it_can_sum_numbers_over_nine
+    cc = CreditCheck.new("4929735477250543")
+    expected = [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8]
+    assert_equal expected, cc.double_every_other_number
+  end
+
+  def test_it_can_sum_the_final_array
+    cc = CreditCheck.new("4929735477250543")
+    assert_equal 80, cc.sum_final_array
+  end
+
+  def test_can_can_check_card_validation
+    cc = CreditCheck.new("4929735477250543")
+    assert_equal "Card is valid.", cc.validate
+  end
+
+
 
 
 
